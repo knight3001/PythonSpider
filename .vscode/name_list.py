2958101b4927ab6955239a649935b2d12e7a43f8
Terry <knight3001@aliyun.com>
2017-02-14 09:21:25 +1000

    aus name list file

---
+++ .vscode/name_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# pylint: disable=C0103,C0111
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    ls = ""
+    url = "http://www.babynamewizard.com/name-list/australian-boys-names-most-popular-names-for-boys-in-australia-new-south-wales"
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, "html.parser")
+    list_soup = soup.find('ol', attrs={'class':'no-icon'})
+    for li in list_soup.find_all('li'):
+        try:
+            name = li.find('a').get_text()
+        except AttributeError:
+            name = li.get_text()
+        ls += "'" + name + "',"
+    print(ls)
+
+if __name__ == '__main__':
+    main()
